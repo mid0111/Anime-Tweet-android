8c56d23f5e545fa9f81e5bbe91219d2ab5cc0132
mid0111 <m.tajima0111@gmail.com>
2014-04-27 18:07:38 +0900

    Create project.

---
+++ app/src/main/java/com/mid/anime_tweet_android/app/ListViewAdapter.java
@@ -0,0 +1,35 @@
+package com.mid.anime_tweet_android.app;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+/**
+ * Created by mid on 14/04/27.
+ */
+public class ListViewAdapter extends ArrayAdapter<String> {
+    private final Context context;
+    private final ArrayList<String> values;
+
+    public ListViewAdapter(Context context, ArrayList<String> values) {
+        super(context, R.layout.item_tweet, values);
+        this.context = context;
+        this.values = values;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context
+                .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(R.layout.item_tweet, parent, false);
+        TextView textView = (TextView) rowView.findViewById(R.id.textView);
+        textView.setText(values.get(position));
+
+        return rowView;
+    }
+}
---
+++ app/src/main/java/com/mid/anime_tweet_android/app/MainActivity.java
@@ -0,0 +1,54 @@
+package com.mid.anime_tweet_android.app;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final ListView listview = (ListView) findViewById(R.id.listView);
+        String[] values = new String[] { "Android", "iPhone", "WindowsMobile",
+                "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
+                "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
+                "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
+                "Android", "iPhone", "WindowsMobile" };
+
+        final ArrayList<String> list = new ArrayList<String>();
+        for (int i = 0; i < values.length; ++i) {
+            list.add(values[i]);
+        }
+
+        final ListViewAdapter adapter = new ListViewAdapter(this, list);
+        listview.setAdapter(adapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+}
